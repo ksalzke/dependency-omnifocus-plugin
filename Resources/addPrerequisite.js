423903cffce37923d153859a0baf62c993b295a2
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-09-22 00:33:59 +0100

    Convert to plugin

---
+++ Resources/addPrerequisite.js
@@ -0,0 +1,55 @@
+/*{
+	"type": "action",
+	"targets": ["omnifocus"],
+	"author": "Kaitlin Salzke",
+	"identifier": "com.KaitlinSalzke.addPrerequisite",
+	"version": "1.0",
+	"description": "Tag selected tag as 'Waiting: Other task' and adds link in its note to task currently tagged 'Make Prerequisite'; then removes that tag from the prequisite task, replaces it with the actual prerequisite tag and updates the note of the prerequisite task with a link. NOTE: Currently only deals with one tagged task.",
+	"label": "Add Prerequisite",
+	"shortLabel": "Add Prerequisite"
+}*/
+var _ = (function() {
+  var action = new PlugIn.Action(function(selection, sender) {
+    // configure tags
+    markerTag = tagNamed("Make Prerequisite");
+    prerequisiteTag = tagNamed("🔑");
+    dependantTag = tagNamed("Activity Type")
+      .tagNamed("⏳ Waiting")
+      .tagNamed("🔒 Other task");
+
+    task = selection.tasks[0];
+
+    // GET PREREQUISITE
+    // get all tasks tagged with 'prerequisite'
+    prereqTask = markerTag.tasks[0];
+    prereqTaskId = prereqTask.id.primaryKey;
+
+    // DEAL WITH SELECTED (DEPENDENT) NOTE
+    task.addTag(dependantTag); // add waiting tag to selected note
+    task.note =
+      "[PREREQUISITE: omnifocus:///task/" +
+      prereqTaskId +
+      "] " +
+      prereqTask.name +
+      "\n\n" +
+      task.note; // prepend prerequisite details to selected note
+
+    // DEAL WITH PREREQUISITE TASK
+    prereqTask.addTag(prerequisiteTag); // add tag to prerequisite
+    prereqTask.note =
+      "[DEPENDANT: omnifocus:///task/" +
+      task.id.primaryKey +
+      "] " +
+      task.name +
+      "\n\n" +
+      prereqTask.note; // prepend dependant details to prerequisite note
+    prereqTask.removeTag(markerTag); // remove marker tag used for processing;
+  });
+
+  action.validate = function(selection, sender) {
+    return selection.tasks.length === 1;
+  };
+
+  return action;
+})();
+_;
---
+++ Resources/checkPrerequisites.js
@@ -0,0 +1,97 @@
+/*{
+	"type": "action",
+	"targets": ["omnifocus"],
+	"author": "Kaitlin Salzke",
+	"identifier": "com.KaitlinSalzke.checkPrerequisites",
+	"version": "1.0",
+	"description": "Check prerequisites",
+	"label": "Check Prerequisites",
+	"shortLabel": "Check Prerequisites"
+}*/
+var _ = (function() {
+  var action = new PlugIn.Action(function(selection, sender) {
+    // config
+    dependantTag = tagNamed("Activity Type")
+      .tagNamed("⏳ Waiting")
+      .tagNamed("🔒 Other task");
+    prerequisiteTag = tagNamed("🔑");
+
+    // get all remaining tasks that are waiting on prerequisites
+    remainingTasks = [];
+    dependantTag.tasks.forEach(function(task) {
+      if (task.taskStatus === Task.Status.Blocked) {
+        remainingTasks.push(task);
+      }
+    });
+
+    // for each task that is waiting:
+    remainingTasks.forEach(function(dependentTask) {
+      // use regex to find [PREREQUISITE: taskid] matches in the notes and capture task IDs
+      regex = /\[PREREQUISITE: omnifocus:\/\/\/task\/(.+)\]/g;
+      var regexArray = [];
+      prerequisiteTasksArray = [];
+      while ((regexArray = regex.exec(dependentTask.note)) !== null) {
+        // for each captured task ID
+        prerequisiteTaskId = regexArray[1];
+        // get the task with that ID and push to array
+        prerequisiteTag.tasks.forEach(function(task) {
+          if (task.id.primaryKey == prerequisiteTaskId) {
+            prerequisiteTasksArray.push(task);
+            return ApplyResult.Stop;
+          }
+        });
+      }
+
+      // for each prerequsite task that has been captured
+      prerequisiteTasksArray.forEach(function(prerequisiteTask) {
+        // if completed:
+        if (prerequisiteTask.completed) {
+          // use regex to find [DEPENDANT: taskid] matches in the notes and capture task IDs
+          regex = /\[DEPENDANT: omnifocus:\/\/\/task\/(.+)\]/g;
+          var regexArray = [];
+          while ((regexArray = regex.exec(prerequisiteTask.note)) !== null) {
+            // for each captured task ID
+            dependantTaskId = regexArray[1];
+            // get the task with that ID
+            var dependantTask = null;
+            dependantTag.tasks.forEach(function(task) {
+              if (task.id.primaryKey == dependantTaskId) {
+                dependantTask = task;
+                return ApplyResult.Stop;
+              }
+            });
+            // if result is not null, remove the prerequisite tag from the dependant task
+            if (dependantTask !== null) {
+              regexString =
+                "[PREREQUISITE: omnifocus:///task/" +
+                prerequisiteTaskId +
+                "].+";
+              RegExp.quote = function(str) {
+                return str.replace(/([?*^$[\]\\(){}|-])/g, "\\$1");
+              };
+              regexForNoteSearch = new RegExp(RegExp.quote(regexString));
+              dependantTask.note = dependantTask.note.replace(
+                regexForNoteSearch,
+                ""
+              );
+              // check whether any remaining prerequisite tasks listed in the note (i.e. whether all prerequisites completed) - and if so
+              if (!/\[PREREQUISITE:/.test(dependantTask.note)) {
+                // if no remaining prerequisites, remove 'Waiting' tag from dependant task
+                dependantTask.removeTag(dependantTag);
+              }
+            }
+          }
+        }
+      });
+    });
+  });
+
+  action.validate = function(selection, sender) {
+    // validation code
+    // selection options: tasks, projects, folders, tags
+    return true;
+  };
+
+  return action;
+})();
+_;
