37014a2b826d2b459f0213488117f42da67997d0
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-09-22 23:29:28 +0100

    Use function in library to complete check

--- Resources/checkPrerequisites.js
+++ Resources/checkPrerequisites.js
@@ -6,6 +6,8 @@ var _ = (function() {
 			.tagNamed("🔒 Other task");
 		prerequisiteTag = tagNamed("🔑");
 
+		dependencyLibrary = this.dependencyLibrary;
+
 		// get all remaining tasks that are waiting on prerequisites
 		remainingTasks = [];
 		dependantTag.tasks.forEach(function(task) {
@@ -32,53 +34,14 @@ var _ = (function() {
 				});
 			}
 
-			// for each prerequsite task that has been captured
-			prerequisiteTasksArray.forEach(function(prerequisiteTask) {
-				// if completed:
-				if (prerequisiteTask.completed) {
-					// use regex to find [DEPENDANT: taskid] matches in the notes and capture task IDs
-					regex = /\[DEPENDANT: omnifocus:\/\/\/task\/(.+)\]/g;
-					var regexArray = [];
-					while ((regexArray = regex.exec(prerequisiteTask.note)) !== null) {
-						// for each captured task ID
-						dependantTaskId = regexArray[1];
-						// get the task with that ID
-						var dependantTask = null;
-						dependantTag.tasks.forEach(function(task) {
-							if (task.id.primaryKey == dependantTaskId) {
-								dependantTask = task;
-								return ApplyResult.Stop;
-							}
-						});
-						// if result is not null, remove the prerequisite tag from the dependant task
-						if (dependantTask !== null) {
-							regexString =
-								"[PREREQUISITE: omnifocus:///task/" +
-								prerequisiteTaskId +
-								"].+";
-							RegExp.quote = function(str) {
-								return str.replace(/([?*^$[\]\\(){}|-])/g, "\\$1");
-							};
-							regexForNoteSearch = new RegExp(RegExp.quote(regexString));
-							dependantTask.note = dependantTask.note.replace(
-								regexForNoteSearch,
-								""
-							);
-							// check whether any remaining prerequisite tasks listed in the note (i.e. whether all prerequisites completed) - and if so
-							if (!/\[PREREQUISITE:/.test(dependantTask.note)) {
-								// if no remaining prerequisites, remove 'Waiting' tag from dependant task
-								dependantTask.removeTag(dependantTag);
-							}
-						}
-					}
-				}
+			// or each prerequsite task that has been captured
+			prerequisiteTasksArray.forEach(prerequisiteTask => {
+				dependencyLibrary.checkDependants(prerequisiteTask);
 			});
 		});
 	});
 
 	action.validate = function(selection, sender) {
-		// validation code
-		// selection options: tasks, projects, folders, tags
 		return true;
 	};
 
