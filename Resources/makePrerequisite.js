ef70eb4dfb42650f4663cd8dc5fdbad14a503048
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2020-04-05 21:02:29 +1000

    Add makePrerequisite action

---
+++ Resources/makePrerequisite.js
@@ -0,0 +1,21 @@
+var _ = (function() {
+  var action = new PlugIn.Action(function(selection, sender) {
+    config = this.dependencyConfig;
+
+    // configure tags
+    markerTag = config.markerTag();
+
+    tasks = selection.tasks;
+
+    tasks.forEach(function(task) {
+      task.addTag(markerTag);
+    });
+  });
+
+  action.validate = function(selection, sender) {
+    return selection.tasks.length > 0;
+  };
+
+  return action;
+})();
+_;
