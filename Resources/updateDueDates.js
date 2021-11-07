d4897b06bf8b4a607020780edc9d2d3d4ea45f8e
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2021-11-07 22:07:18 +1100

    fix: get updateDueDates working

--- Resources/updateDueDates.js
+++ Resources/updateDueDates.js
@@ -1,54 +1,36 @@
-/* global PlugIn */
+/* global PlugIn Task */
 (() => {
-  const action = new PlugIn.Action(async function (selection, sender) {
-    // config
-    const prereqTag = await this.dependencyLibrary.getPrefTag('dependantTag')
+  const action = new PlugIn.Action(function (selection, sender) {
+    // make sure any old links have been cleared out
+    this.dependencyLibrary.updateDependancies()
 
-    const dependencyLibrary = this.dependencyLibrary
+    // get all dependant tasks
+    const deps = this.dependencyLibrary.getLinks().map(link => Task.byIdentifier(link[1]))
 
-    prereqTag.remainingTasks.forEach((task) => {
-      // for each remaining task tagged as a prerequisite...
+    // limit to those with due dates
+    const dueDeps = deps.filter(d => d.effectiveDueDate !== null)
 
-      // recursively get directly dependant tasks (and tasks dependent on those tasks, etc)
-      const dependentTasks = []
-      const allDeps = preReq => {
-        const depTasks = dependencyLibrary.getDependants(preReq)
-        depTasks.forEach(dep => {
-          dependentTasks.push(dep)
-          if (dep.tags.includes(prereqTag)) {
-            allDeps(dep)
-          }
-        })
-      }
-      allDeps(task)
+    // process each dependent task with due date
+    const updateDate = (tasks, date) => {
+      const tasksToUpdate = tasks.filter(t => t.effectiveDueDate === null || date < t.effectiveDueDate)
+      tasksToUpdate.forEach(t => (t.dueDate = date))
+      return tasksToUpdate
+    }
 
-      // find earliest due date of dependent tasks
-      let earliestDue = null
-      dependentTasks.forEach((dep) => {
-        if (dep.effectiveDueDate !== null && (dep.effectiveDueDate < earliestDue || earliestDue === null)) {
-          earliestDue = dep.effectiveDueDate
+    dueDeps.forEach(dep => {
+      // get a complete list of prerequisites (including indirect ones) and update the date
+      const prereqs = this.dependencyLibrary.getAllPrereqs(dep)
+      const updatedPrereqs = updateDate(prereqs, dep.effectiveDueDate)
+
+      // if part of a sequential action group/project, also update due date of preceding actions
+      updatedPrereqs.forEach(p => {
+        if (p.parent !== null && p.parent.sequential) {
+          const siblings = p.parent.children
+          const pIndex = siblings.indexOf(p)
+          const precedingTasks = siblings.slice(0, pIndex)
+          updateDate(precedingTasks, dep.effectiveDueDate)
         }
       })
-
-      if (
-        (earliestDue <= task.effectiveDueDate && earliestDue !== null) ||
-        task.effectiveDueDate == null
-      ) {
-        // if earlier than task's current effective due date, use earlier date
-        if (earliestDue < task.effectiveDueDate) {
-          task.dueDate = earliestDue
-        }
-        // if prerequisite task is part of a sequential action group/project, also update due date of preceding actions
-        if (task.parent !== null && task.parent.sequential) {
-          const siblings = task.parent.children
-          for (let i = 0; i < siblings.indexOf(task); i++) {
-            const sibling = siblings[i]
-            if (sibling.effectiveDueDate === null || earliestDue < sibling.effectiveDueDate) {
-              sibling.dueDate = earliestDue
-            }
-          }
-        }
-      }
     })
   })
 
