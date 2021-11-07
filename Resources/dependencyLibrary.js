c338f46908c80ac3c056e4c319c770b1074769a9
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2021-11-07 22:11:40 +1100

    refactor: move updateDueDates into lib

--- Resources/dependencyLibrary.js
+++ Resources/dependencyLibrary.js
@@ -147,5 +147,39 @@
     linksToRemove.forEach(link => dependencyLibrary.removeDependancy(link[0], link[1]))
   }
 
+  dependencyLibrary.updateDueDates = () => {
+    // make sure any old links have been cleared out
+    dependencyLibrary.updateDependancies()
+
+    // get all dependant tasks
+    const deps = dependencyLibrary.getLinks().map(link => Task.byIdentifier(link[1]))
+
+    // limit to those with due dates
+    const dueDeps = deps.filter(d => d.effectiveDueDate !== null)
+
+    // process each dependent task with due date
+    const updateDate = (tasks, date) => {
+      const tasksToUpdate = tasks.filter(t => t.effectiveDueDate === null || date < t.effectiveDueDate)
+      tasksToUpdate.forEach(t => (t.dueDate = date))
+      return tasksToUpdate
+    }
+
+    dueDeps.forEach(dep => {
+      // get a complete list of prerequisites (including indirect ones) and update the date
+      const prereqs = dependencyLibrary.getAllPrereqs(dep)
+      const updatedPrereqs = updateDate(prereqs, dep.effectiveDueDate)
+
+      // if part of a sequential action group/project, also update due date of preceding actions
+      updatedPrereqs.forEach(p => {
+        if (p.parent !== null && p.parent.sequential) {
+          const siblings = p.parent.children
+          const pIndex = siblings.indexOf(p)
+          const precedingTasks = siblings.slice(0, pIndex)
+          updateDate(precedingTasks, dep.effectiveDueDate)
+        }
+      })
+    })
+  }
+
   return dependencyLibrary
 })()
--- Resources/updateDueDates.js
+++ Resources/updateDueDates.js
@@ -1,37 +1,7 @@
-/* global PlugIn Task */
+/* global PlugIn */
 (() => {
   const action = new PlugIn.Action(function (selection, sender) {
-    // make sure any old links have been cleared out
-    this.dependencyLibrary.updateDependancies()
-
-    // get all dependant tasks
-    const deps = this.dependencyLibrary.getLinks().map(link => Task.byIdentifier(link[1]))
-
-    // limit to those with due dates
-    const dueDeps = deps.filter(d => d.effectiveDueDate !== null)
-
-    // process each dependent task with due date
-    const updateDate = (tasks, date) => {
-      const tasksToUpdate = tasks.filter(t => t.effectiveDueDate === null || date < t.effectiveDueDate)
-      tasksToUpdate.forEach(t => (t.dueDate = date))
-      return tasksToUpdate
-    }
-
-    dueDeps.forEach(dep => {
-      // get a complete list of prerequisites (including indirect ones) and update the date
-      const prereqs = this.dependencyLibrary.getAllPrereqs(dep)
-      const updatedPrereqs = updateDate(prereqs, dep.effectiveDueDate)
-
-      // if part of a sequential action group/project, also update due date of preceding actions
-      updatedPrereqs.forEach(p => {
-        if (p.parent !== null && p.parent.sequential) {
-          const siblings = p.parent.children
-          const pIndex = siblings.indexOf(p)
-          const precedingTasks = siblings.slice(0, pIndex)
-          updateDate(precedingTasks, dep.effectiveDueDate)
-        }
-      })
-    })
+    this.dependencyLibrary.updateDueDates()
   })
 
   action.validate = function (selection, sender) {
